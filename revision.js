db7f4f4773d855d2822f5eb937267a9ba4ecb6ea
Maasuuum <fardousalmasum09@gmail.com>
2024-02-09 02:51:32 +0600

    revision file

---
+++ revision.js
@@ -0,0 +1,35 @@
+// string type variable
+var bottle = 'water';
+var bottleColor = 'yellow';
+var penColor = 'blue';
+
+// number type variable
+var sunglassPrice = 120;
+
+// boolean type variable
+var isExpensive = true;
+
+// array declaration
+var tableItems = ['bottle', 'pen', 'mouse', 'sunglass', 'oil', 'laptop'];
+
+// show array item
+var item3 = tableItems[3];
+
+// change array item by index no
+tableItems[4] = 'notebook';
+
+// specific items index no
+var mouseIndex = tableItems.indexOf('pen');
+
+// push pop
+
+// conditionals
+if(tableItems.length > 4){
+
+}
+else if(tableItems[3] == 'pen'){
+
+}
+else{
+    
+}
