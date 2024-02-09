39972c4c40199186fedb69f12ede955068b264d4
Maasuuum <fardousalmasum09@gmail.com>
2024-02-09 17:09:55 +0600

    while loop

---
+++ while.js
@@ -0,0 +1,21 @@
+// number finding by while loop
+var myine = 0;
+while(myine < 7){
+    console.log('roast den')
+    console.log(myine);
+    myine++;
+}
+
+// even number
+var number = 0;
+while(number <= 10){
+    console.log(number);
+    number = number + 2;
+}
+
+// odd number
+var oddNumber = 1;
+while(oddNumber <=10){
+    console.log(oddNumber);
+    oddNumber = oddNumber + 2;
+}
